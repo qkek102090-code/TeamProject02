4474aa7f862bbfe9e08b46b421fd6d39467a7e93
Trued <qkek102090@gmail.com>
2026-03-24 17:21:46 +0900

    [Mine] MyActor 클래스 수정

--- Source/TeamProject02/MyActor02.cpp
+++ Source/TeamProject02/MyActor02.cpp
@@ -1,24 +1,18 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// MyActor02.cpp
 
 
 #include "MyActor02.h"
 
-// Sets default values
+
 AMyActor02::AMyActor02()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
-
-// Called when the game starts or when spawned
 void AMyActor02::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
-
-// Called every frame
 void AMyActor02::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
